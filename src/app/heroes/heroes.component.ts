856b2b622ea7a56ec95af92a95f33a62c37b10bb
kimphysicsman <kimphysicsman@gmail.com>
2022-12-26 23:29:13 +0900

    feat/ 양방향 바인딩 구현

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -10,5 +11,8 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  hero = 'Superman';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
 }
